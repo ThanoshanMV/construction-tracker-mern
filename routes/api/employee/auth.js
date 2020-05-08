b48e55d88b6928261806127d87fdffd27bdfa0b9
ThanoshanMV <mvthanoshan9@gmail.com>
2020-05-08 17:21:04 +0530

    employee authentication Login route

--- routes/api/employee/auth.js
+++ routes/api/employee/auth.js
@@ -5,6 +5,11 @@
 const express = require('express');
 //use express router
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+
 //Get the auth middleware
 const auth = require('../../../middleware/auth');
 
@@ -13,7 +18,7 @@ const User = require('../../../models/employee/User');
 //create route
 
 // @route         GET api/employee/auth
-// @description   Test route
+// @description   Protected route
 // @access        Public
 
 //We can add auth middleware as a second parameter.
@@ -27,6 +32,84 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route         POST api/employee/auth
+// @description   Authenticate user & get token
+// @access        Public
+
+router.post(
+  '/',
+  [
+    //Using express-validator to validate inputs
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    //if there are any errors..
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    /**
+     * req.body is the object of data that's going be sent.
+     * So we can access req.body.name , req.body.email and so on. Instead
+     * of accessing them one by one, we can do like this below..
+     * */
+    const { email, password } = req.body;
+
+    /*We're using async, so we must wrap our code inside try-catch block and await.
+     * Anything that return a promise make sure to put async before
+     */
+
+    try {
+      //See if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        //User does not exist
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //User exists
+
+      // Check whether the password matches
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        //Password does not match
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //Password matches
+
+      //Create payload
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      //Generate token
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 }, //expires in 360000 seconds
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token }); // Send the token to the client
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 //export the route
 module.exports = router;
