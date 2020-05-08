ad356785e2bd2c1c23427e485b0a3eb7f89924dc
ThanoshanMV <mvthanoshan9@gmail.com>
2020-05-08 20:08:16 +0530

    admin Profile model & route for getting current admin's profile

--- routes/api/admin/profile.js
+++ routes/api/admin/profile.js
@@ -5,15 +5,37 @@
 const express = require('express');
 //use express router
 const router = express.Router();
+const auth = require('../../../middleware/auth');
+
+const User = require('../../../models/admin/User');
+const Profile = require('../../../models/admin/Profile');
 
 //create route
 
-// @route         GET api/admin/profile
-// @description   Test route
-// @access        Public
+// @route         GET api/admin/profile/me
+// @description   Get current users profile
+// @access        Private
 
-//if the access in public you dont need jsonwebtokens (dont require authorizations)
-router.get('/', (req, res) => res.send('Admin Profile route'));
+/*access is Private, so it's a protected route. 
+Thus we add auth here (Should be 2nd parameter)*/
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    //find profile using id (id taken from token)
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('admin', ['name', 'avatar']);
+    // Profile does not exist
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    // Profile exists
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 //export the route
 module.exports = router;
