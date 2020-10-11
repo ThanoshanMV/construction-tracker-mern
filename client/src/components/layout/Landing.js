ca33e6c1f6fa7b31d3b9cf2024a7c499faee2a01
ThanoshanMV <mvthanoshan9@gmail.com>
2020-10-11 19:42:03 +0530

    changing UI colors

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -14,10 +14,12 @@ import React from 'react';
 
 const Landing = () => {
   return (
-    <section className='landing'>
-      <div className='landing-inner'>
-        <h1 className='x-large'>Welcome to Construction Tracker</h1>
-        <p className='lead'>Track Construction Status with Ease!</p>
+    <section class='landing'>
+      <div class='dark-overlay'>
+        <div class='landing-inner'>
+          <h1 class='x-large'>Welcome to Construction Tracker</h1>
+          <p class='lead'>Track Construction Status with Ease!</p>
+        </div>
       </div>
     </section>
   );
